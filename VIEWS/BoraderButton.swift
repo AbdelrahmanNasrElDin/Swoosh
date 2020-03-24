7d2a285bb96617bba130403abe7422f16cfe7429
Al-fredo07 <abdelrahman.nasreldin.student@pua.edu.eg>
2020-03-24 23:23:37 +0200

    add button to wellcome page

---
+++ VIEWS/BoraderButton.swift
@@ -0,0 +1,23 @@
+//
+//  BoraderButton.swift
+//  Swoosh
+//
+//  Created by Abd-Elrahman Nasr-Eldin on 3/24/20.
+//  Copyright © 2020 Abd-Elrahman Nasr-Eldin. All rights reserved.
+//
+
+import UIKit
+
+class BoraderButton: UIButton {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+        
+        
+        
+    }
+
+}
