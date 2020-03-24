f67775f10dfa335e0cc50d33417ff62cb1151fbd
Al-fredo07 <abdelrahman.nasreldin.student@pua.edu.eg>
2020-03-24 22:17:18 +0200

    create wellcome page

---
+++ Swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Abd-Elrahman Nasr-Eldin on 3/24/20.
+//  Copyright © 2020 Abd-Elrahman Nasr-Eldin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
